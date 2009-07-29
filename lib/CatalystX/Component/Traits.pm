8213bf50fa91dfbdebae1b4dc1043811fffdbd76
Tomas Doran <bobtfish@bobtfish.net>
2009-07-29 23:40:39 +0100

    Changelog, bump versions

--- lib/CatalystX/Component/Traits.pm
+++ lib/CatalystX/Component/Traits.pm
@@ -15,11 +15,11 @@ Catalyst Components
 
 =head1 VERSION
 
-Version 0.07
+Version 0.08
 
 =cut
 
-our $VERSION   = '0.07';
+our $VERSION   = '0.08';
 our $AUTHORITY = 'id:RKITOVER';
 
 =head1 SYNOPSIS
