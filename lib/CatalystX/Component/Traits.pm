9dff510255bb224e40fcf22ab93661f43c1cc29f
Tomas Doran <tdoran@yelp.com>
2014-01-02 13:33:00 +0000

    Version 0.17

--- lib/CatalystX/Component/Traits.pm
+++ lib/CatalystX/Component/Traits.pm
@@ -16,11 +16,11 @@ Catalyst Components
 
 =head1 VERSION
 
-Version 0.16
+Version 0.17
 
 =cut
 
-our $VERSION   = '0.16';
+our $VERSION   = '0.17';
 our $AUTHORITY = 'id:RKITOVER';
 
 =head1 SYNOPSIS
