c901ecba472eadf2b9053e17ee4049e94976bd46
Rafael Kitover <rkitover@cpan.org>
2014-01-08 06:09:31 -0500

    update POD, remove $VERSION for dzil

--- lib/CatalystX/Component/Traits.pm
+++ lib/CatalystX/Component/Traits.pm
@@ -13,13 +13,8 @@ with 'MooseX::Traits::Pluggable' => { excludes => ['_find_trait'] };
 CatalystX::Component::Traits - Automatic Trait Loading and Resolution for
 Catalyst Components
 
-=head1 VERSION
-
-Version 0.14
-
 =cut
 
-our $VERSION   = '0.14';
 our $AUTHORITY = 'id:RKITOVER';
 
 =head1 SYNOPSIS
@@ -260,41 +255,17 @@ L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=CatalystX-Component-Traits>.  I
 will be notified, and then you'll automatically be notified of progress on your
 bug as I make changes.
 
-=head1 SUPPORT
-
-More information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=CatalystX-Component-Traits>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/CatalystX-Component-Traits>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/CatalystX-Component-Traits>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/CatalystX-Component-Traits/>
-
-=back
-
 =head1 ACKNOWLEDGEMENTS
 
 Matt S. Trout and Tomas Doran helped me with the current design.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright (c) 2009, Rafael Kitover
+Copyright (c) 2014, Rafael Kitover
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
 =cut
 
-1; # End of CatalystX::Component::Traits
+__PACKAGE__; # End of CatalystX::Component::Traits
